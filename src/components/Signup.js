72419508e9ffa71f5547ba5896631b3fc79aec3b
Saurabh Kumar Mishra <saurabhmishra31234@gmail.com>
2022-09-12 18:54:15 +0530

    changes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,6 +4,9 @@ function Signup() {
   return (
     <div>Signup
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia eaque rerum dignissimos magni nobis cupiditate provident dolor assumenda obcaecati saepe, veniam aperiam voluptatibus aut, id omnis est aspernatur! Nostrum, sed?</p>
+    <h1>
+      This is the signup form for the user account  
+    </h1>
     </div>
   )
 }
